b5ac5e8250b3a57449185d15a608a8a5593062ee
Mortar1on <mortarion018@gmail.com>
2020-10-26 11:03:56 +0200

    Initial commit

---
+++ src/test/java/com/example/notebook/demo/NotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notebook.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
