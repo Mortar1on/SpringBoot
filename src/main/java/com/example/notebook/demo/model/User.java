989ce18eedd9bb9bcab9805a478d4f005eb894a5
Mortar1on <mortarion018@gmail.com>
2020-10-26 11:21:35 +0200

    An object representation of the User entity in the database

---
+++ src/main/java/com/example/notebook/demo/model/User.java
@@ -0,0 +1,23 @@
+package com.example.notebook.demo.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+}
