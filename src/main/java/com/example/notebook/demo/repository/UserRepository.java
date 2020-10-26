4e7a132a500b7ff9aac85da3bf0a1523a8aa7d50
Mortar1on <mortarion018@gmail.com>
2020-10-26 11:31:17 +0200

    Spring Data

---
+++ src/main/java/com/example/notebook/demo/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.notebook.demo.repository;
+
+import com.example.notebook.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Long, User> {
+}
